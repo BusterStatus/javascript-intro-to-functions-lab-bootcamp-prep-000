98c0711535fdf09db18a6050a9c051d55d640e70
matthew.serre@gmail.com <matthew.serre@gmail.com>
2018-09-12 00:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,16 +17,9 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string === "hello") {
     return "I can't hear you!";
+  } else if (string === "HELLO"){
+    return "YES INDEED!";
+  } else {
+    return "I love you, too.";
   }
-    it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
 }
